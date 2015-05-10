49239dea004a664f0388cb3b5f1be3bef728cf3f
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2015-05-10 10:37:59 +0100

    change rsync target in upload.sh
    nimbus is dead long live ashbrook

--- bin/upload.sh
+++ bin/upload.sh
@@ -2,4 +2,4 @@
 # script to generate and upload capitalbeards.org.uk to nimbus
 rm -rf /tmp/cbjekyllbuild
 bundle exec jekyll build -d /tmp/cbjekyllbuild
-rsync -avzPhc --stats /tmp/cbjekyllbuild/* nimbus.geekcloud.com:web/vhosts/capitalbeards.org.uk/
+rsync -avzPhc --stats /tmp/cbjekyllbuild/* ashbrook.default.rjw1.uk0.bigv.io:/srv/capitalbeards.org.uk/public/htdocs/
